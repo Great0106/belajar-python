edbcf1bf498eb0dcb5d8a12af074ae05181672d7
Great0106 <raufderiel@gmail.com>
2022-10-12 15:06:09 +0700

    fourth commit

---
+++ functions.py
@@ -0,0 +1,29 @@
+def multiplication(a,b):
+    multiply = a*b
+    print(str(a)+" x "+str(b)+" = "+str(multiply))
+
+multiplication(2,7)
+multiplication(105,5)
+multiplication(5,27)
+multiplication(2,3)
+multiplication(15,3)
+
+def subtraction(c,d):
+    subtract = c-d
+    print(str(c)+" - "+str(d)+" = "+str(subtract))
+
+subtraction(5,2)
+subtraction(7,5)
+subtraction(10,9)
+subtraction(100,5)
+subtraction(2000,1)
+
+def division(e,f):
+    divide = e/f
+    print(str(e)+" / "+str(f)+" = "+str(divide))
+
+division(10,2)
+division(50,5)
+division(54,6)
+division(1500,15)
+division(96,8)
