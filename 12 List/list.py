ff9b8caefece548820e95643710ec7eadee05e89
Great0106 <raufderiel@gmail.com>
2022-11-03 14:59:44 +0700

    list commit

---
+++ 12 List/list.py
@@ -0,0 +1,6 @@
+months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+print(months[:6])
+# print(len(months))
+# for x in months:
+#    print(x)
