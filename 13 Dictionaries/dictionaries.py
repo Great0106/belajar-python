3e7ae83c33a1c98ee84684ec92b644b867ba3ebe
Great0106 <raufderiel@gmail.com>
2022-11-09 14:54:32 +0700

    list and dictionary

---
+++ 13 Dictionaries/dictionaries.py
@@ -0,0 +1,13 @@
+info = {
+    "name": "Rauf Deriel",
+    "residence": "Serang, Banten",
+    "age": 12
+}
+
+info["birthday"] = "1 Jun 2010"
+info["education"] = "7th Grade"
+
+print(info)
+print(type(info))
+print(info.keys())
+print(info.values())
